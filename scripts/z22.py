bd5be20ca8515f7adedc5ac313992467ecae32b4
Kirill Ponur <ponur0kirill@gmail.com>
2020-03-26 00:38:18 +0300

    pre-final commit

--- scripts/z22.py
+++ scripts/z22.py
@@ -1,4 +1,6 @@
 import numpy as np 
+import scipy as sp
+from scipy import interpolate
 import matplotlib.pyplot as plt 
 import pandas as pd
 fig = plt.figure() 
@@ -7,23 +9,20 @@ ax = fig.add_subplot(111)
 file = pd.read_excel('../rec.xlsx')
 y = file['U, В']
 x = file['I,мА']
-# x=np.array([0,0.2,0.26,0.3,0.34,0.36,0.38,0.4,0.44]) 
-# y=np.array([0,1,8,14,28,38,54,74,100]) 
-# plt.errorbar(x, y, xerr=0.5, yerr=0.01, c='blue', lw=0.5, mfc='white', ms=3) 
+y = np.array(y[0:-1])
+x = np.array(x[0:-1])
+y_false = np.insert(y,9,224)
+x_false = np.insert(x,9,0.032)
+f = interpolate.interp1d(x_false,y_false,kind='quadratic')
 
-
-# plt.errorbar(x, y, xerr=0.5, yerr=0.01, c='black', lw=0.5, mfc='white', ms=3) 
-
-# x=np.array([0,19.8,20,20.5,21,21.5,22,22.5,23,23.5,24,24.5,25,25.5,26,26.5,27]) 
-# y=np.array([0,0.008,0.015,0.022,0.033,0.059,0.071,0.092,0.13,0.17,0.227,0.301,0.375,0.455,0.62,0.74,0.985]) 
-# plt.errorbar(x, y, xerr=0.5, yerr=0.01, c='red', lw=0.5, mfc='white', ms=3) 
-
-
-ax.semilogx(x,y,'ko', color = "brown", markersize=4) 
+x0 = np.linspace(min(x),max(x),10000)
+ax.semilogx(x0,f(x0),color="darkblue",label='сплайн-интерполяция')
+ax.semilogx(x,y,'ko', color = "brown", markersize=4,label='практика') 
 plt.xlabel('$J$, мА') 
 plt.ylabel('$U$,В') 
 plt.grid(which='minor',linestyle=":") 
 plt.grid(which='major',linestyle="-") 
 plt.minorticks_on()
+plt.legend()
 plt.savefig('fig2.pdf', bbox_inches="tight")
 plt.show()
